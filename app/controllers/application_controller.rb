e2c4e10b01ad3e3ebc27d28d34b839bce27a9f3b
elizabethvera-atieno <atienolizvera@gmail.com>
2022-06-26 11:50:50 +0300

    commit tree 0b81fe8d4851d936c9035f79ba299aaa78ea4030

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,38 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  # get '/game/:id' do
+  #   game = Game.find(params[:id])
+  #   game.to_json(include: {reviews: {include: :user}})
+  # end
+
+  # get '/game' do
+  #   game = Game.all.order(:genre).limit(1)
+
+  #   # include associated reviews in the JSON response
+  #   game.to_json(only: [:id, :title, :genre, :price], include: {
+  #     reviews: { only: [:comment, :score], include: {
+  #       user: { only: [:name] }
+  #     } }
+  #   })
+  # end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
